7b9b60539f6fbb43444b74be50fc003a65cf9060
zeelsonii <zeelsonii21@gmail.com>
2023-12-27 18:45:56 +0530

    my first commit

---
+++ ABC.java
@@ -0,0 +1,31 @@
+public class ABC{
+
+	//static int a = 2;
+	//public int b = 10;
+	
+	
+public static void main(String[] args){
+
+	int a = 5;
+	Integer i = a;
+	//System.out.println(a);
+	//System.out.println(this.b);
+	
+//	ABC a1 = new ABC();
+	
+//	a1.a = 200;
+	//a1.b = 20;
+	
+	//System.out.println(a1.a);
+	System.out.println(i);
+	
+	/*
+	ABC a2 = new ABC();
+	//a2.a = 300;
+	a2.b = 30;
+	
+	//System.out.println(a2.a);
+	System.out.println(a2.b);*/
+	
+	}
+}
